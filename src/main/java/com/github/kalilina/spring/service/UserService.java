fef7c9690244a2b4f50b9245d61e0b8b4b5d3628
kalilina <kalilina@protonmail.com>
2025-12-21 16:36:01 +0200

    feat(user): implements user service CRUD methods

--- src/main/java/com/github/kalilina/spring/service/UserService.java
+++ src/main/java/com/github/kalilina/spring/service/UserService.java
@@ -1,16 +1,65 @@
 package com.github.kalilina.spring.service;
 
+import com.github.kalilina.spring.database.repository.CompanyRepository;
 import com.github.kalilina.spring.database.repository.UserRepository;
+import com.github.kalilina.spring.dto.UserCreateEditDto;
+import com.github.kalilina.spring.dto.UserReadDto;
 import com.github.kalilina.spring.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@ToString
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
     private final UserMapper userMapper;
+
+    public List<UserReadDto> findAll() {
+        return userRepository.findAll().stream()
+                .map(userMapper::toReadDto)
+                .toList();
+    }
+
+    public Optional<UserReadDto> findById(Long id) {
+        return userRepository.findById(id)
+                .map(userMapper::toReadDto);
+    }
+
+    @Transactional
+    public UserReadDto save(UserCreateEditDto userCreateDto) {
+        return Optional.ofNullable(userCreateDto)
+                .map(userDto -> userMapper.toEntity(userDto, companyRepository))
+                .map(userRepository::save)
+                .map(userMapper::toReadDto)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public Optional<UserReadDto> update(Long id, UserCreateEditDto userEditDto) {
+        return userRepository.findById(id)
+                .map(userEntity -> {
+                    userMapper.updateEntity(userEditDto, userEntity, companyRepository);
+                    return userEntity;
+                })
+                .map(userRepository::saveAndFlush)
+                .map(userMapper::toReadDto);
+    }
+
+    @Transactional
+    public boolean delete (Long id) {
+        return userRepository.findById(id)
+                .map(entity -> {
+                    userRepository.delete(entity);
+                    userRepository.flush();
+                    return true;
+                })
+                .orElse(false);
+    }
 }
