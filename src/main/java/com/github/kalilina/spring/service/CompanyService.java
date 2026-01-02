3bae706630829fe4d195505ff6d972fbb200cde7
kalilina <kalilina@protonmail.com>
2026-01-02 17:09:37 +0200

    feat: add findAll method and read mode for CompanyService

--- src/main/java/com/github/kalilina/spring/service/CompanyService.java
+++ src/main/java/com/github/kalilina/spring/service/CompanyService.java
@@ -8,16 +8,25 @@ import com.github.kalilina.spring.mapper.CompanyMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
     private final ApplicationEventPublisher applicationEventPublisher;
+
+    public List<CompanyReadDto> findAll() {
+        return companyRepository.findAll().stream()
+                .map(companyMapper::toReadDto)
+                .toList();
+    }
 
     public Optional<CompanyReadDto> findById(int id) {
         return companyRepository
