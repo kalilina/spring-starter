3721a33329f9aa8ffe087a7a9744db13849cfe3f
kalilina <kalilina@protonmail.com>
2026-01-05 17:21:07 +0200

    refactor(repository): replace Criteria API with QueryDSL

--- src/main/java/com/github/kalilina/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/com/github/kalilina/spring/database/repository/FilterUserRepositoryImpl.java
@@ -1,15 +1,16 @@
 package com.github.kalilina.spring.database.repository;
 
 import com.github.kalilina.spring.database.entity.User;
+import com.github.kalilina.spring.dto.QPredicate;
 import com.github.kalilina.spring.dto.UserFilter;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.JoinType;
-import jakarta.persistence.criteria.Predicate;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+
+import static com.github.kalilina.spring.database.entity.QUser.user;
 
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
@@ -18,29 +19,45 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
-        var cb = entityManager.getCriteriaBuilder();
-        var criteria = cb.createQuery(User.class);
+        Predicate predicate = filter.personalInfo() == null
+                ? null
+                : QPredicate.builder()
+                .add(filter.personalInfo().firstname(), user.personalInfo.firstname::containsIgnoreCase)
+                .add(filter.personalInfo().lastname(), user.personalInfo.lastname::containsIgnoreCase)
+//                    .add(filter.personalInfo().birthDate(), user.personalInfo.birthDate::eq) // не хочу переделывать entity PersonalInfo
+                                                                                               // (нужно поле Birthday заменить на LocalDate)
+                .buildAnd();
 
-        var user = criteria.from(User.class);
-        criteria.select(user);
+        return new JPAQuery<User>(entityManager)
+                .select(user)
+                .from(user)
+                .where(predicate)
+                .fetch();
 
-        List<Predicate> predicates = new ArrayList<>();
-        if (filter.personalInfo() != null) {
-            if (filter.personalInfo().firstname() != null && !filter.personalInfo().firstname().isBlank()) {
-                predicates.add(cb.like(user.get("personalInfo").get("firstname"), filter.personalInfo().firstname()));
-            }
-            if (filter.personalInfo().lastname() != null && !filter.personalInfo().lastname().isBlank()) {
-                predicates.add(cb.like(user.get("personalInfo").get("lastname"), filter.personalInfo().lastname()));
-            }
-            if (filter.personalInfo().birthDate().birthday() != null) {
-                predicates.add(cb.lessThan(
-                        user.get("personalInfo").get("birthDate").as(LocalDate.class),
-                        filter.personalInfo().birthDate().birthday()));
-            }
-        }
-
-        criteria.where(predicates.toArray(Predicate[]::new));
-
-        return entityManager.createQuery(criteria).getResultList();
+        // using Criteria API
+//        var cb = entityManager.getCriteriaBuilder();
+//        var criteria = cb.createQuery(User.class);
+//
+//        var user = criteria.from(User.class);
+//        criteria.select(user);
+//
+//        List<Predicate> predicates = new ArrayList<>();
+//        if (filter.personalInfo() != null) {
+//            if (filter.personalInfo().firstname() != null && !filter.personalInfo().firstname().isBlank()) {
+//                predicates.add(cb.like(user.get("personalInfo").get("firstname"), filter.personalInfo().firstname()));
+//            }
+//            if (filter.personalInfo().lastname() != null && !filter.personalInfo().lastname().isBlank()) {
+//                predicates.add(cb.like(user.get("personalInfo").get("lastname"), filter.personalInfo().lastname()));
+//            }
+//            if (filter.personalInfo().birthDate().birthday() != null) {
+//                predicates.add(cb.lessThan(
+//                        user.get("personalInfo").get("birthDate").as(LocalDate.class),
+//                        filter.personalInfo().birthDate().birthday()));
+//            }
+//        }
+//
+//        criteria.where(predicates.toArray(Predicate[]::new));
+//
+//        return entityManager.createQuery(criteria).getResultList();
     }
 }
