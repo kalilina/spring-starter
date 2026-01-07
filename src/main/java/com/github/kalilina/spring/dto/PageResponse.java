d3945da82377bf23344d960a8005b04c709ab86a
kalilina <kalilina@protonmail.com>
2026-01-07 11:27:52 +0200

    feat: add generic PageRespone for paginated API responses

---
+++ src/main/java/com/github/kalilina/spring/dto/PageResponse.java
@@ -0,0 +1,25 @@
+package com.github.kalilina.spring.dto;
+
+import lombok.Value;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Value
+public class PageResponse<T> {
+    List<T> content;
+    Metadata metadata;
+
+    public static <T> PageResponse<T> of(Page<T> page) {
+        List<T> content = page.getContent();
+        Metadata metadata = new Metadata(page.getNumber(), page.getSize(), page.getTotalElements());
+        return new PageResponse<>(content, metadata);
+    }
+
+    @Value
+    private static class Metadata {
+        int page;
+        int size;
+        long totalElements;
+    }
+}
