7c07c0cda3bf67b61aeda1f51bff081b319253f3
kalilina <kalilina@protonmail.com>
2026-01-11 18:50:38 +0200

    feat(user): add new Role

--- src/main/java/com/github/kalilina/spring/database/entity/Role.java
+++ src/main/java/com/github/kalilina/spring/database/entity/Role.java
@@ -3,7 +3,7 @@ package com.github.kalilina.spring.database.entity;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER, ADMIN;
+    USER, ADMIN, OPERATOR;
 
     @Override
     public String getAuthority() {
